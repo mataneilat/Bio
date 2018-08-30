263455ccbd31c6e9775fed3a7f77b4725355bfc1
Matan Eilat <mtneilat@gmail.com>
2018-08-30 03:06:21 +0300

    Documented the contact map repository

--- contact_map_repository.py
+++ contact_map_repository.py
@@ -1,8 +1,19 @@
-
+"""
+    This module contains the contact map repository and its parsers
+"""
 import numpy as np
 import os
 
+
 def parse_gcnn_file(atoms_count, gcnn_file):
+    """
+    Parses a gcnn file that is received as a part of RaptorX's output.
+    The gcnn file is simply a matrix in text format.
+
+    :param atoms_count: The number of atoms to define the returned matrix dimensions.
+    :param gcnn_file:   The path to the gcnn file
+    :return:    A matrix representing the probabilities of each two residues being in contact.
+    """
     if gcnn_file is None:
         return None
     if not os.path.isfile(gcnn_file):
@@ -19,6 +30,13 @@ def parse_gcnn_file(atoms_count, gcnn_file):
 
 
 def parse_rr_file(atoms_count, rr_file):
+    """
+    Parses an rr file that is received as a part of RatporX's output
+    :param atoms_count: The number of atoms to define the returned matrix dimensions.
+    :param rr_file:     The path to the gcnn file
+    :return:    A matrix representing the probabilities of each two residues being in contact.
+                Pairs of residues for which no contact information is given are filled with -1
+    """
     if rr_file is None:
         return None
     if not os.path.isfile(rr_file):
@@ -39,15 +57,38 @@ def parse_rr_file(atoms_count, rr_file):
 
     return mat
 
-class ContactMapRepository:
 
+class ContactMapRepository:
+    """
+    The repository contact maps.
+    The repository works in a lazy manner, meaning that files are only parsed when requested.
+    """
     def __init__(self, contact_map_directory):
+        """
+        Constructs the repository with the directory containing the contact map files.
+
+        :param contact_map_directory:   The directory containing the contact map files.
+        """
         self.contact_map_directory = contact_map_directory
 
     def get_contact_map_gcnn(self, morph_id, residue_count):
+        """
+        Parses and returns the contact map corresponding to the given morph id, given as a .gcnn file.
+
+        :param morph_id:    The morph for which contact map is required.
+        :param residue_count:   The protein's residue count.
+        :return:    The contact map of the protein represented by the given morph id.
+        """
         return parse_gcnn_file(residue_count, os.path.join(self.contact_map_directory, morph_id + '.gcnn'))
 
     def get_contact_map_rr(self, morph_id, residue_count):
+        """
+        Parses and returns the contact map corresponding to the given morph id, given as an .rr file.
+
+        :param morph_id:    The morph for which contact map is required.
+        :param residue_count:   The protein's residue count.
+        :return:    The contact map of the protein represented by the given morph id.
+        """
         return parse_rr_file(residue_count, os.path.join(self.contact_map_directory, morph_id + '.rr'))
 
 
